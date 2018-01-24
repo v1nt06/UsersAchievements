0dac1aa2bff3d83e2120a68c42e65dcb9d0ac451
Ivan Karimov <v1nt06@inbox.ru>
2018-01-24 18:27:56 +0400

    Added templates and breadcrumbs

--- UsersAchievements/Global.asax.cs
+++ UsersAchievements/Global.asax.cs
@@ -1,18 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace UsersAchievements
 {
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : HttpApplication
     {
         protected void Application_Start()
         {
+            AddBundles();
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+
+        private void AddBundles()
+        {
+            var styleBundle = new StyleBundle("~/Content/css");
+            styleBundle.Include("~/Content/bootstrap.css");
+            styleBundle.Include("~/Content/Site.css");
+
+            var scriptBundle = new ScriptBundle("~/Scripts/js");
+            scriptBundle.Include("~/Scripts/jquery-{version}.js");
+            scriptBundle.Include("~/Scripts/bootstrap.js");
+
+            BundleTable.Bundles.Add(styleBundle);
+            BundleTable.Bundles.Add(scriptBundle);
+
+            BundleTable.EnableOptimizations = false;
         }
     }
 }
